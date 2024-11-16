dbde2ef5f0aaacd29138cae4c1226690352764e0
ljs7143 <junseo27643779@gmail.com>
2024-11-16 22:44:28 +0900

    Initial commit

---
+++ src/main/java/com/example/makit/MakitApplication.java
@@ -0,0 +1,13 @@
+package com.example.makit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/makit/MakitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
